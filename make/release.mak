5e6b5c3eb9899ff771c98b382c329e4ca38695ec
Bryan Jurish <jurish@bbaw.de>
2009-07-06 09:57:51 +0000

    git-svn-id: file:///home/svn/dev/dta-tokwrap/trunk@2918 4a515de8-beb3-11dc-84d6-2b654028afac

---
+++ make/release.mak
@@ -0,0 +1,5 @@
+include User.mak
+
+##-- release
+xmldir = ./release
+xml    = $(wildcard $(xmldir)/*.aligned.xml)
--- make/zzz-makeall.sh
+++ make/zzz-makeall.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
-echo "$0: make all extra" 1>&2
-exec make all extra
+#!/bin/bash
+
+test -z "$*" && args=("config=release.mak") || args=("$@")
+
+echo "$0: make ${args[@]} all extra" 1>&2
+exec make "${args[@]}" all extra
