4376a9af9d62d7595b05d8f64ab1eaafb00df17d
Bryan Jurish <jurish@bbaw.de>
2012-01-12 10:03:53 +0000

    + faster regex-based dtatw-dtaid.sh
    git-svn-id: file:///home/svn/dev/dta-tokwrap/trunk@6508 4a515de8-beb3-11dc-84d6-2b654028afac

--- scripts/dtatw-dtaid.sh
+++ scripts/dtatw-dtaid.sh
@@ -9,6 +9,8 @@ dir0=$(dirname "$0")
 for f in "$@"; do
   b=$(basename "$f")
   b=${b%%.*}
-  dtaid=$("$dir0"/dtatw-get-header.perl -e='idno' -a type=DTAID "$f" | perl -pe 's|^[^\>]*>\s*||; s|\s*\<.*$||;')
+  #dtaid=$("$dir0"/dtatw-get-header.perl -e='idno' -a type=DTAID "$f" | perl -pe 's|^[^\>]*>\s*||; s|\s*\<.*$||;')
+  dtaid=$(perl -ne 'if (m(\<\s*idno\b[^\>]*?\btype=\"DTAID\"[^\>]*?\>\s*([^\<\s]*?)\s*\</idno\>)i) { print $1; exit 0; }' "$f")
+  test -z "$dtaid" && dtaid=-
   echo -e "$f\t$b\t$dtaid"
 done
