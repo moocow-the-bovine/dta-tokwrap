9641bbfe9a80d71f9f3d69296f30e4bdd25b28d7
Bryan Jurish <jurish@bbaw.de>
2013-06-21 08:11:14 +0000

    + http tokenizer: changed default url host back to kaskade.dwds.de (now -> services2)
    git-svn-id: file:///home/svn/dev/dta-tokwrap/trunk@9963 4a515de8-beb3-11dc-84d6-2b654028afac

--- DTA-TokWrap/TokWrap/Processor/tokenize/http.pm
+++ DTA-TokWrap/TokWrap/Processor/tokenize/http.pm
@@ -38,7 +38,9 @@ sub defaults {
   my $that = shift;
   return (
 	  $that->SUPER::defaults(),
-	  serverurl => 'http://194.95.188.28/dtaos/tok-cgi.perl',
+	  #serverurl => 'http://194.95.188.28/dtaos/tok-cgi.perl', ##-- kaskade1
+          #serverurl => 'http://194.95.188.59/dtaos/tok-cgi.perl', ##-- kaskade2
+          serverurl => 'http://kaskade.dwds.de/dtaos/tok-cgi.perl',
 	  txtparam  => 't',
 	  timeout   => 300,
 	  @_,
